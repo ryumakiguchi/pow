50ef389b1352305eb06f98344878f853f60ef8ab
Ryusei Makiguchi <makiguchir@gmail.com>
2022-10-14 16:28:56 -0300

    landing

--- app/javascript/controllers/dog_controller.js
+++ app/javascript/controllers/dog_controller.js
@@ -1,37 +1,44 @@
   const results = document.querySelector("#results")
 
-function dogFetch(){
-  fetch("https://pow-application.herokuapp.com/api/v1/dogs")
+function golferFetch(){
+  fetch("https://pow-application.herokuapp.com/api/v1/golfers")
   .then(response => response.json())
   .then((data) => {
     console.log(data)
     data.forEach((result) => {
-      const dogTag = `<li class="list-inline-item cards">
-        <p>${result.name}</p>
-        <p>${result.breed}</p>
-      </li>`
-      results.insertAdjacentHTML("beforeend", dogTag)
+      const golferTag =
+      `<div class="card rounded" style="width: 24rem;">
+        <div class="card-body">
+          <h5 class="card-title">Golfer: ${result.name}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Handicap: ${result.handicap}</h6>
+          <p class="card-text">Comment: ${result.review}</p>
+        </div>
+      </div>`
+
+      results.insertAdjacentHTML("beforeend", golferTag)
     })
   })
 }
-dogFetch()
+golferFetch()
 
-  const addDog = (event) => {
+  const addGolfer = (event) => {
     event.preventDefault()
     const nameValue = document.getElementById("name").value
-    const breedValue = document.getElementById("breed").value
-    fetch("https://pow-application.herokuapp.com/api/v1/dogs", {
+    const handicapValue = document.getElementById("handicap").value
+    const reviewValue = document.getElementById("review").value
+
+    fetch("https://pow-application.herokuapp.com/api/v1/golfers", {
       method: "POST",
       headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({"name": nameValue, "breed": breedValue})
+      body: JSON.stringify({"name": nameValue, "handicap": handicapValue, "review": reviewValue})
     })
       .then(response => response.json())
       .then((data) => {
         console.log(data)
       })
       results.innerHTML = ""
-      dogFetch();
+      golferFetch();
   }
 
   const form = document.querySelector("#form")
-  form.addEventListener("submit", addDog)
+  form.addEventListener("submit", addGolfer)
